39e718a8c12ff6f901a516a9fbd7972cb050fece
Edunno <tamericandream@hotmail.com>
2019-02-25 10:59:53 +0100

    demo1

--- src/main/java/dbfacades/DemoFacade.java
+++ src/main/java/dbfacades/DemoFacade.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /*
 Simple Facade demo for this start-up project
@@ -14,53 +15,89 @@ Simple Facade demo for this start-up project
   - Delete the three public methods below, and replace with your own Facade Logic 
   - Delete all content in the main method
 
-*/
+ */
 public class DemoFacade {
 
-  EntityManagerFactory emf;
+    EntityManagerFactory emf;
 
-  public DemoFacade(EntityManagerFactory emf) {
-    this.emf = emf;
-  }
-
-  public Car addCar(Car car){
-    EntityManager em = emf.createEntityManager();
-    try{
-      em.getTransaction().begin();
-      em.persist(car);
-      em.getTransaction().commit();
-      return car;
-    }finally{
-      em.close();
-    }
-  }
-  
-  public List<Car> getAllCars() {
-   throw new UnsupportedOperationException("FIX ME, but write the test first");
-  }
-  public long countCars() {
-    EntityManager em = emf.createEntityManager();
-    try {
-      return (Long) em.createQuery("select Count(m) from Car m").getSingleResult();
-    } finally {
-      em.close();
+    public DemoFacade(EntityManagerFactory emf) {
+        this.emf = emf;
     }
 
-  }
+    public Car addCar(Car car) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(car);
+            em.getTransaction().commit();
+            return car;
+        } finally {
+            em.close();
+        }
+    }
 
-  /*
+    public List<Car> getAllCars() {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("Select c from Car c");
+        return q.getResultList();
+    }
+
+    public long countCars() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return (Long) em.createQuery("select Count(m) from Car m").getSingleResult();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    /*
   This will only work when your have added a persistence.xml file in the folder: 
      src/main/resources/META-INF
   You can use the file: persistence_TEMPLATE.xml (in this folder) as your template
-  */
-  public static void main(String[] args) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    DemoFacade df = new DemoFacade(emf);
-    df.addCar(new Car("Volvo"));
-    df.addCar(new Car("WW"));
-    df.addCar(new Car("Jaguar"));
-    long numbOfCars = df.countCars();
-    System.out.println("Number of cars: "+numbOfCars);
-  }
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade df = new DemoFacade(emf);
+        df.addCar(new Car("Volvo"));
+        df.addCar(new Car("WW"));
+        df.addCar(new Car("Jaguar"));
+        long numbOfCars = df.countCars();
+        System.out.println("Number of cars: " + numbOfCars);
+    }
+
+    public Car getCarsByMake(String make) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query q = em.createQuery("select c from Car c where c.make = :make").setParameter("make", make);
+            return (Car) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Car getCarByID(String string) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Car a = em.find(Car.class,
+                    Integer.parseInt(string));
+            return a;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteCarByID(int id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createQuery("delete from Car where id = :id").setParameter("id", id).executeUpdate();
+            em.getTransaction().commit();
+            
+        } finally {
+            em.close();
+        }
+    }
 
 }
--- src/main/java/utils/Tester.java
+++ src/main/java/utils/Tester.java
@@ -1,11 +1,53 @@
 package utils;
 
+import dbfacades.DemoFacade;
+import entity.Car;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-public class Tester {
-  
-  public static void main(String[] args) {
-    System.out.println("Building the Table(s)");
-    Persistence.generateSchema("pu", null);
-  }
 
+public class Tester {
+
+    public static void main(String[] args) {
+        Tester t = new Tester();
+        t.deleteCarByIDTest();
+    }
+
+    public void getAllCarsTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade d = new DemoFacade(emf);
+        List<Car> cars = d.getAllCars();
+        System.out.println("All Cars: " + cars.get(1) + " Array Size: " + cars.size());
+    }
+
+    public void getCarsByMakeTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade d = new DemoFacade(emf);
+        Car a = d.getCarsByMake("WW");
+        System.out.println("Car by make: " + a.getMake());
+    }
+
+    public void getCarsByIDTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade d = new DemoFacade(emf);
+        Car a = d.getCarByID("1");
+        System.out.println("Car with ID 1: " + a.getMake());
+    }
+
+    public void deleteCarByIDTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade d = new DemoFacade(emf);
+        int id = 1;
+        d.deleteCarByID(id);
+        try{
+            d.getCarByID(Integer.toString(id));
+        }
+        catch(Exception e){
+            System.out.println("No such car. Removal worked.");
+        }
+        Car a = new Car("Volvo");
+        a.setId(id);
+        d.addCar(a);
+    }
 }
